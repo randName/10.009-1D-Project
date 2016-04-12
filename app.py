8b0be18255741bc2855689bb1f726c6ee2c63ff6
Shun Git <kwokshungit@gmail.com>
2016-04-12 19:52:15 +0800

    create main app

---
+++ app.py
@@ -0,0 +1,22 @@
+import time
+
+from hardware import Curtain, LDR, cleanup
+
+def main():
+    print "Running"
+
+if __name__ == "__main__":
+    print "J.A.R.V.I.S. Activated"
+    
+    refresh_interval = 5
+    last_time = 0
+
+    try:
+        while True:
+            if time.time() - last_time > refresh_interval:
+                main()
+                last_time = time.time()
+    except KeyboardInterrupt:
+        print "J.A.R.V.I.S. Deactivated"
+    finally:
+        cleanup()
